ae8bb561180ab697fe4e6c9d7e0c5820daf08388
insilicomab <54536339+insilicomab@users.noreply.github.com>
2022-01-30 17:46:02 +0900

    Initial commit

---
+++ api/serializers.py
@@ -0,0 +1,24 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import User
+from .models import Task
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M", read_only=True)
+
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'created_at', 'updated_at')
